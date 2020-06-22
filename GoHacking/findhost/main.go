86f665e63e5d5c4772a491c9729403325d2be97e
jjacob151 <jayne_jacobs@cable.comcast.com>
2020-06-22 18:28:11 -0400

    findhost

--- GoHacking/findhost/main.go
+++ GoHacking/findhost/main.go
@@ -12,7 +12,7 @@ import (
 func main() {
 
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Minute)
-	targetIP := "192.168.0.1/24"
+	targetIP := "192.168.221.1/24"
 
 	defer cancel()
 	scanner, err := nmap.NewScanner(
@@ -26,28 +26,32 @@ func main() {
 	}
 
 	results, warning, err := scanner.Run()
+
 	if err != nil {
 		log.Fatal(err)
 	}
-	
+
 	if warning != nil {
-		log.Fatal("warning %s", warning)
+		log.Fatalf("warning %s \n", warning)
 	}
 
-	for _, host := range results.Hosts{
-		if len(host.Ports)==0 {
-			if len(host.Ports)==0 || len(host.Addresses)==0{
+	for _, host := range results.Hosts {
+		if len(host.Ports) == 0 {
+			if len(host.Ports) == 0 || len(host.Addresses) == 0 {
 				continue
-			}		
+			}
 
-			if len(host.Ports)>1 {
+			fmt.Printf("IP: %q\n", host.Addresses)
+
+			if len(host.Addresses) > 1 {
 				fmt.Printf("IP: %q", host.Addresses[0])
 			}
 
 			for _, port := range host.Ports {
-					fmt.Printf("\t Port %s", port.ID)
+				fmt.Printf("\t Port %d %s %s %s\n", port.ID, port.Protocol, port.State, port.Service.Name)
 			}
-	
+
 		}
 	}
+
 }
